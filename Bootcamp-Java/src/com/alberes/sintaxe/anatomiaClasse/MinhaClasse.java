a2be9f08031ee4efbfd909dfee26301d59a8a3f2
alberes <alberessilva@yahoo.com.br>
2025-02-13 11:55:21 -0300

    Apresentação de boas vindas ao curso

---
+++ Bootcamp-Java/src/com/alberes/sintaxe/anatomiaClasse/MinhaClasse.java
@@ -0,0 +1,8 @@
+package com.alberes.sintaxe.anatomiaClasse;
+
+public class MinhaClasse {
+    public static void main (String[] args){
+        System.out.println("Olá, sejam bem vindos ao curso de Java");
+    } 
+    
+}
