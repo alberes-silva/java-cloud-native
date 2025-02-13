288d265ece4e0079c31075d0f301abf7a3ed2cb1
alberes <alberessilva@yahoo.com.br>
2025-02-13 11:23:15 -0300

    Atualização dos tipos de variavéis

--- Tipos-Variaveis/src/App.java
+++ Tipos-Variaveis/src/App.java
@@ -1,7 +1,10 @@
 public class App {
-    public static void main(String[] args) {
-        //int, byte, short, long, float, double, boolean e char
+    public static void main(String[] args) throws Exception{
+        //int, byte, short, long -> Números inteiros. Ex.: 3
+        //float, double -> Números fracionados. Ex.: 3.14
+        //boolean ->  Lógico. Ex.: true ou false
+        //String, char -> Texto. Ex.: "Texto completo", 'R'
 
-        
+
     }
 }
