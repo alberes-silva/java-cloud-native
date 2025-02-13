d2056180c8bce87405f2ec7d6680f62492afebb1
alberes <alberessilva@yahoo.com.br>
2025-02-13 14:07:22 -0300

    Operações aritmétricos e operador de atribuição

---
+++ Bootcamp-Java/src/com/alberes/sintaxe/operadores/Operadores.java
@@ -0,0 +1,30 @@
+package com.alberes.sintaxe.operadores;
+
+public class Operadores {
+    public static void main(String[] args) {
+        //Operador de atribuição ( = )
+
+        //Ex.: 
+        //String nome = "Alberes Silva";
+        //int numero = 20;
+        //char sexo = 'M';
+        //Date daraNascimento = new Date(); -> classes
+
+        //Operadores Aritmétricos + (soma), - (subtração), * (Multiplicação), / (divisão)
+
+        double resultado = (54 * 5) + (21 - 7) / 2;
+        System.out.println(resultado);
+
+
+        String nome = "Alberes";
+        String sobreNome = "Silva";
+        String nomeCompleto = nome + " " + sobreNome;
+        System.out.println(nomeCompleto);
+
+
+
+
+
+
+    }
+}
