13fe64b19ceecc2d3b2dd887ba096cb107791421
alberes <alberessilva@yahoo.com.br>
2025-02-13 11:51:19 -0300

    Utilização do if - else

--- Bootcamp-Java/src/com/alberes/sintaxe/anatomiaClasse/BoletimEducacional.java
+++ Bootcamp-Java/src/com/alberes/sintaxe/anatomiaClasse/BoletimEducacional.java
@@ -1,6 +1,16 @@
 package com.alberes.sintaxe.anatomiaClasse;
 public class BoletimEducacional {
     public static void main(String[] args) {
+
+        int mediaFinal = 8;
+
+        if(mediaFinal < 6){
+            System.out.println("Reprovado");
+        } else if(mediaFinal == 6){
+            System.out.println("Recuperação");
+        } else {
+            System.out.println("Aprovado");
+        }
         
     }
 }
