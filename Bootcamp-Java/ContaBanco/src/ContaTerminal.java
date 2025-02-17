e0825180d260de1f8b1ed4ac5c259ca40ab367d7
alberes <alberessilva@yahoo.com.br>
2025-02-17 16:10:50 -0300

    Configutração do scanner e da saída de dados

--- Bootcamp-Java/ContaBanco/src/ContaTerminal.java
+++ Bootcamp-Java/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a sua agencia: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite a sua conta: ");
+        int conta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+
+        System.out.println("Olá "+ nome + ", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+conta+" e seu saldo R$ "+saldo+"  já está disponível para saque.");
+        
     }
 }
