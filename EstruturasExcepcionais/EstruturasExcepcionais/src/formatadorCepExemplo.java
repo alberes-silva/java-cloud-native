817536c99e7db78b2b4ae76fa2f2ba9b3c01832b
alberes <alberessilva@yahoo.com.br>
2025-02-18 16:54:05 -0300

    Exemplos de Exceções

---
+++ EstruturasExcepcionais/EstruturasExcepcionais/src/formatadorCepExemplo.java
@@ -0,0 +1,15 @@
+public class formatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2365064");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+           System.out.println("O cep não corresponde com as regras de negócio");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8){
+            throw new CepInvalidoException();
+        }return "23.765-064";
+    }
+}
