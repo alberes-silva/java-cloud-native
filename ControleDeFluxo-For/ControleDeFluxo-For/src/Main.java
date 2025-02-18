fa14530cd6886d092ceca090086603e7c1df77fd
alberes <alberessilva@yahoo.com.br>
2025-02-18 14:21:00 -0300

    Exemplos de for e For Each

---
+++ ControleDeFluxo-For/ControleDeFluxo-For/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        for (int carneirinho = 1; carneirinho <= 20; carneirinho++){
+            if(carneirinho == 1){
+                System.out.println(carneirinho + "- carneirinho");
+            } else {
+                System.out.println(carneirinho + "- carneirinhos");
+            }
+        }
+        System.out.println("Joãozinho dormiu");
+    }
+}
