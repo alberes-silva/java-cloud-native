78bb5064bcf47f30d8705b4d14976394ed6a48a3
alberes <alberessilva@yahoo.com.br>
2025-02-20 21:48:35 -0300

    Create Main.java

---
+++ POO/Escola/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Estudante aluno = new Estudante();
+		
+		boolean livro = aluno.ler();
+		System.out.println("O estudante ler livros? " + livro);
+
+	}
+
+}
