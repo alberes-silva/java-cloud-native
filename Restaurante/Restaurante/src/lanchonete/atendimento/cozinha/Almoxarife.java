a1fd43abec21e4ccf8609ec55e835e7b9caa33ef
alberes <alberessilva@yahoo.com.br>
2025-02-20 21:49:26 -0300

    Create Almoxarife.java

---
+++ Restaurante/Restaurante/src/lanchonete/atendimento/cozinha/Almoxarife.java
@@ -0,0 +1,20 @@
+package lanchonete.atendimento.cozinha;
+
+public class Almoxarife {
+	public void controlarEntrada() {
+		System.out.println("Controlando a entrada dos itens");
+	}
+	
+	public void controlarSaida() {
+		System.out.println("Controlando a saída dos itens");
+	}
+	
+	void entregarIngredientes() {
+		System.out.println("Entregando ingredientes");
+	}
+	
+	void trocarGas() {
+		System.out.println("Almoxarife trocando o gas");
+	}
+
+}
