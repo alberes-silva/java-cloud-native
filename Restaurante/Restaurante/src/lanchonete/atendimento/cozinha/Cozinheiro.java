40aaccb6cfeb2dae6e49d51eced56e18769320c6
alberes <alberessilva@yahoo.com.br>
2025-02-27 13:46:26 -0300

    Update Cozinheiro.java

--- Restaurante/Restaurante/src/lanchonete/atendimento/cozinha/Cozinheiro.java
+++ Restaurante/Restaurante/src/lanchonete/atendimento/cozinha/Cozinheiro.java
@@ -1,7 +1,5 @@
 package lanchonete.atendimento.cozinha;
 
-import lanchonete.atendimento.Atendente;
-
 public class Cozinheiro {
 	
 	public void adicionarLancheNoBalcao() {
@@ -17,48 +15,45 @@ public class Cozinheiro {
 		adicionarSucoNoBalcao();
 	}
 	
-	public void prepararLanche() {
+	private void prepararLanche() {
 		System.out.println("Preparando lanche tipo hamburger");
 	}
 	
-	public void prepararVitamina() {
+	private void prepararVitamina() {
 		System.out.println("Preparando suco");
 	}
 	
-	public void prepararCombo() {
+	private void prepararCombo() {
 		prepararLanche();
 		prepararVitamina();
 	}
 	
-	public void selecionarIngredientesLanche() {
+	private void selecionarIngredientesLanche() {
 		System.out.println("Selecionado o pão, salada, ovo e carne");
 	}
 	
-	public void selecionarIngredientesVitamina() {
+	private void selecionarIngredientesVitamina() {
 		System.out.println("Selecionado fruta. leite e suco");
 	}
 	
-	public void lavarIngredientes() {
+	private void lavarIngredientes() {
 		System.out.println("Lavando ingredientes");
 	}
 	
-	public void baterVitaminaLiquidificador() {
+	private void baterVitaminaLiquidificador() {
 		System.out.println("Batendo vitamina liquificador");
 	}
 	
-	public void fritarIngredientesLanche() {
+	private void fritarIngredientesLanche() {
 		System.out.println("Fritando a carne e ovo para o hamburger");
 	}
 	
-	public void pedirParaTrocarGas(Atendente meuAmigo) {
+	
+	private void pedirParaTrocarGas(Almoxarife meuAmigo) {
 		meuAmigo.trocarGas();
 	}
 	
-	public void pedirParaTrocarGas(Almoxarife meuAmigo) {
-		meuAmigo.trocarGas();
-	}
-	
-	public void pedirIngredientes(Almoxarife almoxarife) {
+	private void pedirIngredientes(Almoxarife almoxarife) {
 		almoxarife.entregarIngredientes();
 	}
 
