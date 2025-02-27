209cf002eed7912c0711598111773413c09c6c1e
alberes <alberessilva@yahoo.com.br>
2025-02-27 13:46:17 -0300

    Update Atendente.java

--- Restaurante/Restaurante/src/lanchonete/atendimento/Atendente.java
+++ Restaurante/Restaurante/src/lanchonete/atendimento/Atendente.java
@@ -2,10 +2,11 @@ package lanchonete.atendimento;
 
 public class Atendente {
 	public void servindoMesa() {
+		pegarLancheCozinha();
 		System.out.println("Servindo mesa");
 	}
 	
-	public void pegarLancheCozinha() {
+	private void pegarLancheCozinha() {
 		System.out.println("Pegando o lanche na cozinha");
 	}
 	
@@ -13,11 +14,11 @@ public class Atendente {
 		System.out.println("Recebendo pagamento");
 	}
 	
-	public void trocarGas() {
+	void trocarGas() {
 		System.out.println("Atendente trocando o gas");
 	}
 	
-	public void pegarPedidoBalcao() {
+	private void pegarPedidoBalcao() {
 		System.out.println("Pegando o pedido no balcão");
 	}
 }
