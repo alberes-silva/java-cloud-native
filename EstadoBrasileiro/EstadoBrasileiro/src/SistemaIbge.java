e3f5fd127d4b8ee9ee62bc0dfeada3b28d49caca
alberes <alberessilva@yahoo.com.br>
2025-02-27 14:57:26 -0300

    Create SistemaIbge.java

---
+++ EstadoBrasileiro/EstadoBrasileiro/src/SistemaIbge.java
@@ -0,0 +1,18 @@
+
+public class SistemaIbge {
+
+	public static void main(String[] args) {
+		for(EstadoBrasileiro e: EstadoBrasileiro.values()) {
+			System.out.println(e.getIbge() + " - " + e.getSigla() + " : " + e.getNome());
+		}
+		
+		EstadoBrasileiro eb = EstadoBrasileiro.PIAUI;
+		
+		System.out.println(eb.getNome());
+		System.out.println(eb.getSigla());
+		System.out.println(eb.getNomeMaisculo());
+		System.out.println(eb.getIbge());
+
+	}
+
+}
