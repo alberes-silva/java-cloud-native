bc3a0d43b697b4f12f8f61b196461d4df9529b42
alberes <alberessilva@yahoo.com.br>
2025-02-27 13:46:28 -0300

    Update Estabelecimento.java

--- Restaurante/Restaurante/src/lanchonete/Estabelecimento.java
+++ Restaurante/Restaurante/src/lanchonete/Estabelecimento.java
@@ -1,6 +1,7 @@
 package lanchonete;
 
 import lanchonete.area.cliente.Cliente;
+
 import lanchonete.atendimento.Atendente;
 import lanchonete.atendimento.cozinha.Almoxarife;
 import lanchonete.atendimento.cozinha.Cozinheiro;
@@ -11,46 +12,20 @@ public class Estabelecimento {
 		
 		Cozinheiro cozinheiro = new Cozinheiro();
 		
-		cozinheiro.lavarIngredientes();
-		cozinheiro.baterVitaminaLiquidificador();
-		cozinheiro.selecionarIngredientesLanche();
-		cozinheiro.prepararLanche();
-		cozinheiro.prepararVitamina();
-		cozinheiro.prepararCombo();
-		
-		cozinheiro.adicionarSucoNoBalcao();
+	    cozinheiro.adicionarSucoNoBalcao();
 		cozinheiro.adicionarLancheNoBalcao();
 		cozinheiro.adicionarComboNoBalcao();
 		
-		Almoxarife almoxarife = new Almoxarife();
-		
-		almoxarife.controlarEntrada();
-		almoxarife.controlarSaida();
-		
-		almoxarife.entregarIngredientes();
-		almoxarife.trocarGas();
-		
 		Atendente atendente = new Atendente();
 		
-		atendente.pegarLancheCozinha();
-		atendente.receberPagamento();
 		atendente.servindoMesa();
-		
-		atendente.trocarGas();
+		atendente.receberPagamento();
 		
 		Cliente cliente = new Cliente();
 		
 		cliente.escolherLanche();
 		cliente.fazerPedido();
 		cliente.pagarConta();
-		
-		cliente.pegarPedidoBalcao();
-		
-		cliente.consultarSaldoAplicativo();
-		
-		
-		cozinheiro.pedirParaTrocarGas(atendente);
-		cozinheiro.pedirIngredientes(almoxarife);
-		
+			
 	}	
 }
